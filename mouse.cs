3f42a98dfeece16e7a90f127a7608533732c5fa0
ahkalama <116187665+ahkalama@users.noreply.github.com>
2024-04-06 01:13:43 +0300

    mouse.cs

--- mouse.cs
+++ mouse.cs
@@ -20,15 +20,12 @@ public class MouseLook : MonoBehaviour
 
     void Update()
     {
-        // Fare hareketlerini al
         float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
-        // Y ekseninde oyuncuyu döndür
         yRotation += mouseX;
         transform.localRotation = Quaternion.Euler(0f, yRotation, 0f);
 
-        // X ekseninde kamerayı döndür
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         playerBody.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
